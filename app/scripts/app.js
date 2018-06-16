5a51807ca8e1fb94bbcb2173fa6aa626a7e1f31b
hakimi <imedhkimi@outlook.fr>
2018-06-16 09:36:23 +0200

    initial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularJs1App
+ * @description
+ * # angularJs1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularJs1App', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularJs1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularJs1App
+ */
+angular.module('angularJs1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
